c808e0326a1bd3db987aaf98d099b7bf23dbd77c
pablo <pablomoya211@gmail.com>
2024-05-29 19:49:31 -0300

    agregados los botones de las constantes

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,19 +4,20 @@ use crate::trigonometricas;
 
 pub fn create_buttons(grid: &gtk::Grid, entry: &gtk::Entry) {
     let buttons = [
-        ("ln", 1, 1, 0, 3),("log", 1, 1, 1, 3),("C", 1, 1, 3, 1),("⏻", 1, 1, 4, 1),
-        ("^", 1, 1, 0, 4),("√", 1, 1, 1, 4),("sin", 1, 1, 2, 4),("cos", 1, 1, 3, 4),("tan",1,1,4,4),
+        ("C", 1, 1, 3, 1),("⏻", 1, 1, 4, 1),
+        ("𝑙𝑛", 1, 1, 0, 3),("𝑙𝑜𝑔", 1, 1, 1, 3),("𝑒", 1,1,2,3),( "π", 1, 1, 3, 3),
+        ("^", 1, 1, 0, 4),("√", 1, 1, 1, 4),("𝑠𝑖𝑛", 1, 1, 2, 4),("𝑐𝑜𝑠", 1, 1, 3, 4),("𝑡𝑎𝑛",1,1,4,4),
         ("7", 1, 1, 0, 5),("8", 1, 1, 1, 5),("9", 1, 1, 2, 5),("(", 1, 1, 3, 5),(")",1,1,4,5),
         ("4", 1, 1, 0, 6),("5", 1, 1, 1, 6),("6", 1, 1, 2, 6),("x", 1, 1, 3, 6),("/", 1, 1, 4, 6),
         ("1", 1, 1, 0, 7),("2", 1, 1, 1, 7),("3", 1, 1, 2, 7),("+", 1, 1, 3, 7),("-",1,1,4,7),
-        ("0", 1, 1, 0, 8),(".", 1, 1, 1, 8),("(-)", 1, 1, 2, 8),("=",2,1,3,8)
+        ("0", 1, 1, 0, 8),(".", 1, 1, 1, 8),("( - )", 1, 1, 2, 8),("=",2,1,3,8)
         
     ];
 
     for &(label, width, height, col, row) in &buttons {
         let button = if label == "^" {
             let label = gtk::Label::new(None);
-            label.set_markup("x<sup>y</sup>"); 
+            label.set_markup("𝑥<sup>𝑦</sup>"); 
             let button = gtk::Button::new();
             button.add(&label);
             button
@@ -29,9 +30,9 @@ pub fn create_buttons(grid: &gtk::Grid, entry: &gtk::Entry) {
 }
 
 pub fn style_button(button: &gtk::Button, label: &str) {
-    if ["/", "x", "-", "+", "=", "(", ")", "^", "√", "ln", "log", "sin", "cos", "tan"].contains(&label) {
+    if ["/", "x", "-", "+", "=", "(", ")", "^", "√", "𝑙𝑛", "𝑙𝑜𝑔", "𝑠𝑖𝑛", "𝑐𝑜𝑠", "𝑡𝑎𝑛", "𝑒", "π"].contains(&label) {
         button.style_context().add_class("operation");
-    } else if label == "C" {
+    } else if label == "C" || label == "( - )" {
         button.style_context().add_class("clear");
     } else if label == "⏻" {
         button.style_context().add_class("power");
@@ -57,9 +58,9 @@ fn attach_button(
         let new_text = match label_clone.as_str() {
             "=" => evaluate_expression(&text),
             "C" => String::new(),
-            "sin" => trigonometricas::sin(evaluate_expression(&text).parse().unwrap_or(0.0)).to_string(),
-            "cos" => trigonometricas::cos(evaluate_expression(&text).parse().unwrap_or(0.0)).to_string(),
-            "tan" => trigonometricas::tan(evaluate_expression(&text).parse().unwrap_or(0.0)).to_string(),
+            "𝑠𝑖𝑛" => trigonometricas::sin(evaluate_expression(&text).parse().unwrap_or(0.0)).to_string(),
+            "𝑐𝑜𝑠" => trigonometricas::cos(evaluate_expression(&text).parse().unwrap_or(0.0)).to_string(),
+            "𝑡𝑎𝑛" => trigonometricas::tan(evaluate_expression(&text).parse().unwrap_or(0.0)).to_string(),
              "√" => {
                 let result = evaluate_expression(&text).parse::<f64>().unwrap_or(0.0).sqrt();
                 if result.is_nan() || result.is_infinite() {
@@ -67,6 +68,15 @@ fn attach_button(
                 } else {
                     result.to_string()
                 }
+            },
+            "( - )" => {
+                let current_text = entry_clone.text().to_string();
+                if current_text.starts_with('-') {
+                    entry_clone.set_text(&current_text[1..]);
+                } else {
+                    entry_clone.set_text(&format!("-{}", current_text));
+                }
+                entry_clone.text().to_string()
             },
             _ => format!("{}{}", text, label_clone),
         };
@@ -92,7 +102,7 @@ pub fn handle_key_press(key: &gdk::EventKey, entry: &gtk::Entry) {
         _ => {
             if let Some(character) = keyval.to_unicode() {
                 let character_str = character.to_string();
-                if character.is_digit(10) || "+-*/().lnlog^⏻".contains(character_str.as_str()) {
+                if character.is_digit(10) || "+-*/().𝑙𝑛𝑙𝑜𝑔^⏻".contains(character_str.as_str()) {
                     entry.set_text(&format!("{}{}", entry_text, character_str));
                 }
             }
